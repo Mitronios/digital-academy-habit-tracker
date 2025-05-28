3c3f0b39b8f71608d5fc59eb49f4bf115ea3ad91
Miguel Angel Gonzales <miguelangel.gonzales@assuresoft.tech>
2025-05-28 11:40:42 -0400

    wip: add icons on HabitCard for edit and delete and set up edit modal.

--- src/components/HabitCard.tsx
+++ src/components/HabitCard.tsx
@@ -1,5 +1,7 @@
-import {Card, CardHeader, CardBody, Text} from "@chakra-ui/react"
+import {Card, CardHeader, CardBody, Text, HStack, Box, IconButton, useDisclosure} from "@chakra-ui/react"
 import ProgressTracking from "./ProgressTracking";
+import { FaEdit, FaTrash } from "react-icons/fa";
+import EditHabitModal from "./EditHabitModal";
 
 interface HabitCardProps {
   habitName: string;
@@ -12,31 +14,71 @@ interface HabitCardProps {
 }
 
 const HabitCard = ({habitName, color, habitId, onEdit, onDelete, onReset, onResetComplete}: HabitCardProps) => {
+  // Chakra modal handlers
+  const editDisclosure = useDisclosure();
+
+
   return (
-    <Card bg="white"
-    marginBottom={2}
-    boxShadow="dark-lg"
+    <Box 
+      position="relative"
     >
-      <CardHeader pl="5" bg={color}>
-        <Text fontSize="20px" 
-        fontWeight={"bold"} 
-        textAlign="start"
-        textShadow='1px 1px rgb(83, 82, 82)'
-        marginBottom={3}
-        textColor="#433E28"
-        >
-          {habitName}
-          </Text>
-      </CardHeader>
-      <CardBody>
-        <ProgressTracking 
-        onReset={false} 
-        onResetComplete={() => {}} 
-        habitId={habitId}
+      <Card bg="white"
+      marginBottom={2}
+      boxShadow="dark-lg"
+      >
+        <CardHeader pl="5" bg={color}>
+          <Text fontSize="20px" 
+          fontWeight={"bold"} 
+          textAlign="start"
+          textShadow='1px 1px rgb(83, 82, 82)'
+          marginBottom={3}
+          textColor="#433E28"
+          >
+            {habitName}
+            </Text>
+        </CardHeader>
+        <CardBody>
+          <ProgressTracking 
+          onReset={false} 
+          onResetComplete={() => {}} 
+          habitId={habitId}
+          />
+          
+        </CardBody>
+      </Card>
+
+      {/*Edit-Delete Modal Buttons*/}
+      <HStack
+        className="actions"
+        position="absolute"
+        top={2}
+        right={2}
+        opacity={0}
+        transition="opacity 0.2s" 
+      >
+        <IconButton aria-label="Edit" 
+          icon={<FaEdit/>} 
+          size="sm"
+          onClick={() => {}}
+
         />
-        
-      </CardBody>
-    </Card>
+        <IconButton aria-label="Delete"
+          icon={<FaTrash/>}
+          colorScheme="red"
+          size="sm"
+          onClick={() => {}}
+        />
+      </HStack>
+
+      {/* Edit Modal */}
+      <EditHabitModal
+        isOpen={editDisclosure.isOpen}
+        onClose={editDisclosure.onClose}
+        initialName=""
+        initialColor=""
+        onSave={() => {}}
+      />
+    </Box>
   );
 };
 
