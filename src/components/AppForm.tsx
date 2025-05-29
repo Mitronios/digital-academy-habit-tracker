b618964872888dd97fb29baa01a1fa182ee98ba4
Miguel Angel Gonzales Melgar <mitronios@gmail.com>
2025-05-29 09:03:47 -0400

    styles: adjusted styles.

--- src/components/AppForm.tsx
+++ src/components/AppForm.tsx
@@ -1,121 +1,128 @@
-import { FormControl, FormLabel, HStack, useToast, VStack } from "@chakra-ui/react"
-import AppInput from "./AppInput"
-import ColorPicker from "./ColorPicker"
-import AppButton from "./AppButton"
+import {
+	FormControl,
+	FormLabel,
+	HStack,
+	useToast,
+	VStack,
+} from "@chakra-ui/react";
+import AppInput from "./AppInput";
+import ColorPicker from "./ColorPicker";
+import AppButton from "./AppButton";
 import { useState } from "react";
 
 // Colors
 export const AVAILABLE_COLORS = [
-  "#FF746C50",
-  "#83CCD2750",
-  "#F68BA250",
-  "#E2CF8850",
-  "#D8B0C850",
+	"#FF746C50",
+	"#8FBC8B80",
+	"#F68BA250",
+	"#E2CF8850",
+	"#D8B0C850",
 ];
 
 // Habits structure
 export interface Habit {
-  id: string;
-  name: string;
-  color: string;
+	id: string;
+	name: string;
+	color: string;
 }
 
 // Form Props
 interface AppFormProps {
-  onAddHabit: (newHabit: Habit) => void;
+	onAddHabit: (newHabit: Habit) => void;
 }
 
-const AppForm = ({onAddHabit}: AppFormProps) => {
+const AppForm = ({ onAddHabit }: AppFormProps) => {
+	// States
+	const [habitName, setHabitName] = useState<string>("");
+	const [PickedColor, setPickedColor] = useState<string>("#F5F4D650");
 
-  // States
-  const [habitName, setHabitName] = useState<string>("")
-  const [PickedColor, setPickedColor] = useState<string>("#F5F4D650")
+	// Toast
+	const toast = useToast();
 
-  // Toast
-  const toast = useToast();
+	// Handler
+	const handleSubmit = (event: React.FormEvent) => {
+		event.preventDefault();
 
+		// Check if input is empty
+		if (habitName.trim() === "") {
+			toast({
+				title: "Ups! Can't add empty habit.",
+				description: "Please enter a new habit.",
+				status: "info",
+				duration: 4000,
+				isClosable: true,
+				position: "top-right",
+			});
+			return;
+		}
 
-  // Handler
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
+		// Define a new habit
+		const newHabit: Habit = {
+			id: crypto.randomUUID(),
+			name: habitName.trim(),
+			color: PickedColor,
+		};
 
-    // Check if input is empty
-    if (habitName.trim() === "") {
-      toast({
-        title: "Ups! Can't add empty habit.",
-        description: "Please enter a new habit.",
-        status: "info",
-        duration: 4000,
-        isClosable: true,
-        position: "top-right",
-    })
-    return;
-    }
+		// Handle the newHabit creation
+		onAddHabit(newHabit);
+		setHabitName("");
+		// Handle color selection
+		setPickedColor("#F5F4D6");
+	};
 
-    // Define a new habit
-    const newHabit: Habit = {
-      id: crypto.randomUUID(),
-      name: habitName.trim(),
-      color: PickedColor
-    }
+	// Component
+	const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setHabitName(event.target.value);
+	};
 
-    // Handle the newHabit creation
-    onAddHabit(newHabit);
-    setHabitName("")
-    // Handle color selection
-    setPickedColor("#F5F4D6")
+	return (
+		<section>
+			<form onSubmit={handleSubmit}>
+				<VStack
+					spacing={4}
+					align="stretch"
+				>
+					<FormControl>
+						<FormLabel
+							htmlFor="habit-name"
+							textColor="#9E905F"
+							textShadow="1px 1px rgb(83, 82, 82)"
+						>
+							Add a new habit:
+						</FormLabel>
+						<AppInput
+							placeholder="e.g. Drink more water"
+							value={habitName}
+							type="text"
+							onChange={handleOnChange}
+							id="habit-name"
+						/>
+					</FormControl>
 
-  };
-
-  // Component
-  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setHabitName(event.target.value);
-  };
-  
-  return (
-    <section>
-      <form onSubmit={handleSubmit}>
-        <VStack spacing={4}
-        align="stretch"
-        >
-          <FormControl>
-            <FormLabel 
-            htmlFor="habit-name"
-            textColor="#9E905F"
-            textShadow='1px 1px rgb(83, 82, 82)'
-            >
-              Add a new habit:
-            </FormLabel>
-            <AppInput
-              placeholder="e.g. Drink more water"
-              value={habitName}
-              type="text"
-              onChange={handleOnChange}
-              id="habit-name"
-            />
-          </FormControl>
-
-          <FormControl>
-          <HStack spacing={4} justify="space-around" flexWrap="wrap">
-            <ColorPicker
-              colors={AVAILABLE_COLORS}
-              selectedColor={PickedColor}
-              onColorSelect={setPickedColor}
-              id="habit-color"
-            />
-            <AppButton
-              type="submit"
-              onClick={() => {}}
-            >
-            Add habit
-          </AppButton>
-          </HStack>
-        </FormControl>
-          
-        </VStack>
-      </form>
-    </section>
-  )
-}
+					<FormControl>
+						<HStack
+							spacing={4}
+							justify="space-around"
+							flexWrap="wrap"
+						>
+							<ColorPicker
+								colors={AVAILABLE_COLORS}
+								selectedColor={PickedColor}
+								onColorSelect={setPickedColor}
+								id="habit-color"
+							/>
+							<AppButton
+								type="submit"
+								onClick={() => {}}
+							>
+								Add habit
+							</AppButton>
+						</HStack>
+					</FormControl>
+				</VStack>
+			</form>
+		</section>
+	);
+};
 
 export default AppForm;
--- src/components/HabitCard.tsx
+++ src/components/HabitCard.tsx
@@ -1,145 +1,164 @@
-import {Card, CardHeader, CardBody, Text, HStack, Box, IconButton, useDisclosure, AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody, AlertDialogFooter, Button} from "@chakra-ui/react"
+import {
+	Card,
+	CardHeader,
+	CardBody,
+	Text,
+	HStack,
+	Box,
+	IconButton,
+	useDisclosure,
+	AlertDialog,
+	AlertDialogOverlay,
+	AlertDialogContent,
+	AlertDialogHeader,
+	AlertDialogBody,
+	AlertDialogFooter,
+	Button,
+} from "@chakra-ui/react";
 import ProgressTracking from "./ProgressTracking";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import EditHabitModal from "./EditHabitModal";
 import { useRef } from "react";
 
 interface HabitCardProps {
-  habitName: string;
-  color: string;
-  habitId: string;
-  progress: boolean[];
-  onProgressChange: (progress: boolean[]) => void;
-  onEdit: (id: string, newName: string, newColor: string) => void;
-  onDelete: (id: string) => void;
+	habitName: string;
+	color: string;
+	habitId: string;
+	progress: boolean[];
+	onProgressChange: (progress: boolean[]) => void;
+	onEdit: (id: string, newName: string, newColor: string) => void;
+	onDelete: (id: string) => void;
 }
 
-const HabitCard = ({habitName, color, habitId, onProgressChange, onEdit, onDelete, progress}: HabitCardProps) => {
+const HabitCard = ({
+	habitName,
+	color,
+	habitId,
+	onProgressChange,
+	onEdit,
+	onDelete,
+	progress,
+}: HabitCardProps) => {
+	// Chakra modal handlers
+	const editDisclosure = useDisclosure();
 
-  // Chakra modal handlers
-  const editDisclosure = useDisclosure();
+	const deleteDisclosure = useDisclosure();
+	const cancelRef = useRef(null);
 
-  const deleteDisclosure = useDisclosure();
-  const cancelRef = useRef(null)
+	// Handler for edit and delete
+	const handleEditSave = (newName: string, newColor: string) => {
+		onEdit(habitId, newName, newColor);
+		editDisclosure.onClose();
+	};
 
-  // Handler for edit and delete
-  const handleEditSave = (newName: string, newColor: string) => {
-    onEdit(habitId, newName, newColor);
-    editDisclosure.onClose();
-  }
+	const handleDeleteHabit = (habitId: string) => {
+		onDelete(habitId);
+		deleteDisclosure.onClose();
+	};
 
-  const handleDeleteHabit = (habitId: string) => {
-    onDelete(habitId);
-    deleteDisclosure.onClose();
-  }
+	return (
+		<Box
+			position="relative"
+			_hover={{ ".actions": { opacity: 1 } }}
+		>
+			<Card
+				bg="white"
+				marginBottom={2}
+				boxShadow="dark-lg"
+				textColor="#444444"
+			>
+				<CardHeader
+					padding="32px"
+					bg={color}
+				>
+					<Text
+						fontSize={{ base: "16px", md: "21px" }}
+						fontWeight={"bold"}
+						textAlign="start"
+						textShadow="1px 1px rgb(83, 82, 82)"
+						marginBottom={3}
+						textColor="#ac7070"
+					>
+						{habitName}
+					</Text>
+				</CardHeader>
 
-  return (
-    <Box 
-      position="relative"
-      _hover={{ '.actions': { opacity: 1 } }}
-    >
-      <Card 
-        bg="white"
-        marginBottom={2}
-        boxShadow="dark-lg"
-      >
-        <CardHeader 
-          padding="32px" 
-          bg={color}
-          >
-          <Text 
-          fontSize={{base: "16px", md: "21px" }}
-          fontWeight={"bold"} 
-          textAlign="start"
-          textShadow='1px 1px rgb(83, 82, 82)'
-          marginBottom={3}
-          textColor="#433E28"
-          >{habitName}
-          </Text>
-        </CardHeader>
-        
-        <CardBody>
-          <ProgressTracking 
-            onProgressChange={onProgressChange}
-            habitId={habitId}
-            progress={progress}
-          />
-        </CardBody>
-      </Card>
+				<CardBody>
+					<ProgressTracking
+						onProgressChange={onProgressChange}
+						habitId={habitId}
+						progress={progress}
+					/>
+				</CardBody>
+			</Card>
 
-      {/*Edit-Delete Modal Buttons*/}
-      <HStack
-        className="actions"
-        position="absolute"
-        top={2}
-        right={2}
-        opacity={{base: 1, md:0}}
-        transition="opacity 0.2s"
-        spacing={2}
-      >
-        <IconButton 
-          aria-label="Edit" 
-          icon={<FaEdit/>}
-          colorScheme="yellow"
-          size={{base: "xs", md: "sm"}}
-          onClick={editDisclosure.onOpen}
-        />
-        <IconButton 
-          aria-label="Delete"
-          icon={<FaTrash/>}
-          colorScheme="red"
-          size={{base: "xs", md: "sm"}}
-          onClick={deleteDisclosure.onOpen}
-        />
-      </HStack>
+			{/*Edit-Delete Modal Buttons*/}
+			<HStack
+				className="actions"
+				position="absolute"
+				top={2}
+				right={2}
+				opacity={{ base: 1, md: 0 }}
+				transition="opacity 0.2s"
+				spacing={2}
+			>
+				<IconButton
+					aria-label="Edit"
+					icon={<FaEdit />}
+					colorScheme="yellow"
+					size={{ base: "xs", md: "sm" }}
+					onClick={editDisclosure.onOpen}
+				/>
+				<IconButton
+					aria-label="Delete"
+					icon={<FaTrash />}
+					colorScheme="red"
+					size={{ base: "xs", md: "sm" }}
+					onClick={deleteDisclosure.onOpen}
+				/>
+			</HStack>
 
-      {/* Edit Modal */}
-      <EditHabitModal
-        isOpen={editDisclosure.isOpen}
-        onClose={editDisclosure.onClose}
-        initialName={habitName}
-        initialColor={color}
-        onSave={handleEditSave}
-      />
+			{/* Edit Modal */}
+			<EditHabitModal
+				isOpen={editDisclosure.isOpen}
+				onClose={editDisclosure.onClose}
+				initialName={habitName}
+				initialColor={color}
+				onSave={handleEditSave}
+			/>
 
-      {/* Delete dialog */}
-      <AlertDialog 
-        isOpen={deleteDisclosure.isOpen}
-        leastDestructiveRef={cancelRef}
-        onClose={deleteDisclosure.onClose}
-      >
-        <AlertDialogOverlay/>
-        <AlertDialogContent>
-          <AlertDialogHeader
-            bg="cadetblue"
-          >
-            Delete Habit
-          </AlertDialogHeader>
-          <AlertDialogBody
-            fontWeight="medium"
-          >
-            Are your sure? This action cannot be undone.
-          </AlertDialogBody>
-          <AlertDialogFooter>
-            <Button
-              colorScheme="blue"
-              marginRight={2}
-              ref={cancelRef}
-              onClick={deleteDisclosure.onClose}
-            >
-              Cancel
-            </Button>
-            <Button
-              colorScheme="pink"
-              onClick={() => handleDeleteHabit(habitId)}
-            >
-              Confirm Delete
-            </Button>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    </Box>
-  );
+			{/* Delete dialog */}
+			<AlertDialog
+				isOpen={deleteDisclosure.isOpen}
+				leastDestructiveRef={cancelRef}
+				onClose={deleteDisclosure.onClose}
+			>
+				<AlertDialogOverlay />
+				<AlertDialogContent>
+					<AlertDialogHeader bg="cadetblue">Delete Habit</AlertDialogHeader>
+					<AlertDialogBody fontWeight="medium">
+						Are your sure? This action cannot be undone.
+					</AlertDialogBody>
+					<AlertDialogFooter>
+						<Button
+							colorScheme="blue"
+							marginRight={2}
+							ref={cancelRef}
+							onClick={deleteDisclosure.onClose}
+						>
+							Cancel
+						</Button>
+						<Button
+							colorScheme="pink"
+							onClick={() => handleDeleteHabit(habitId)}
+						>
+							Confirm Delete
+						</Button>
+					</AlertDialogFooter>
+				</AlertDialogContent>
+			</AlertDialog>
+		</Box>
+	);
 };
 
 export default HabitCard;
